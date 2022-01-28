d121d3e4f4b411182d2ba60d490724da76762f7b
andrian-doterb <andrian.rizki@doterb.com>
2022-01-28 16:31:12 +0700

    Add feature CRUD to Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.all
+    @articles = Article.order(title: :asc)
   end
 
   def show
@@ -11,6 +11,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(params_article)
     if @article.save
@@ -19,6 +23,23 @@ class ArticlesController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params_article)
+      flash[:notice] = 'Article was created successfully.'
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = 'Article was deleted.'
+    redirect_to articles_path, status: :see_other
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   get 'hello', to: 'pages#hello'
-  resources :articles, only: %i[index create new show]
+  resources :articles
 end
